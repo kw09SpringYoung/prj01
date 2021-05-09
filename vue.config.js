8ff420a3ebda4af271d3b3e2a9ac120dc3493167
杨春 <894584322@qq.com>
2021-05-09 13:42:31 +0800

    new1

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports={
     outputDir: 'dist',
-    publicPath: process.env.NODE_ENV === 'production' ? '/prj01/' : '/'
+    publicPath: process.env.NODE_ENV === 'production' ? '/' : '/'
 }
